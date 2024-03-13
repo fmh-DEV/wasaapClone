e6eb7623e89ea1b78c5c469cd3eeb0be5463ced6
fmh-DEV <fm.hildebrand@gmail.com>
2024-03-13 16:24:58 -0300

    0:16:40

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,8 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -46,7 +39,7 @@ export default function RootLayout() {
 function RootLayoutNav() {
   return (
     <Stack>
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='index' options={{ headerShown: false }} />
     </Stack>
   );
 }
