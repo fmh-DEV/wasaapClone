4745a4abbd7b6ecc10be1fe4719a4e8e9a209087
fmh-DEV <fm.hildebrand@gmail.com>
2024-03-13 16:45:45 -0300

    0:19:30

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import React from 'react';
+import welcomeImage from '@/assets/images/welcome.png';
+import Colors from '@/constants/Colors';
+
+//
+const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
 
 const Page = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={{ uri: welcome_image }} style={styles.welcome} />
       <Text>FIRST PAGE!!!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  welcome: {
+    width: '100%',
+    height: 300,
+    marginBottom: 80,
+  },
+  headline: {},
+  description: {},
+  link: {
+    color: Colors.gray,
+  },
+});
 
 export default Page;
