a693740cab55c16eb852bf43b622142d19e805eb
fmh-DEV <fm.hildebrand@gmail.com>
2024-03-14 17:15:41 -0300

    0:36:20

--- app/otp.tsx
+++ app/otp.tsx
@@ -5,10 +5,12 @@ import {
   Platform,
   Linking,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 import React, { useState } from 'react';
 import { useRouter } from 'expo-router';
 import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
   //
@@ -29,6 +31,31 @@ const Page = () => {
         <Text style={styles.description}>
           Wassap will need to verify your account. Carrier charges may apply.
         </Text>
+        <View style={styles.list}>
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>Brazil</Text>
+            <Ionicons name='chevron-forward' size={20} color={Colors.gray} />
+          </View>
+          <View style={styles.separator} />
+        </View>
+        <Text style={styles.legal}>
+          You must be{' '}
+          <Text style={styles.link} onPress={openLink}>
+            at least 16 years old
+          </Text>{' '}
+          to register. Learn how WhatsApp works with the{' '}
+          <Text style={styles.link} onPress={openLink}>
+            Meta Companies
+          </Text>
+          .
+        </Text>
+        {/* */}
+        <TouchableOpacity
+          onPress={sendOTP}
+          style={[styles.button, phoneNumber !== '' ? styles.enabled : null]}
+        >
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
@@ -47,6 +74,52 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
     color: Colors.gray,
+  },
+  list: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    width: '100%',
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 6,
+    marginBottom: 10,
+  },
+  listItemText: {
+    fontSize: 16,
+  },
+  separator: {
+    width: '100%',
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.gray,
+    opacity: 0.4,
+  },
+  legal: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: Colors.gray,
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: '100%',
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: Colors.lightGray,
+    borderRadius: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+    color: '#fff',
+  },
+  buttonText: {
+    color: Colors.gray,
+    fontSize: 22,
+    fontWeight: '500',
   },
 });
 
