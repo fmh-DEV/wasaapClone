b2aced90ffadc859a2cd4636b7905b9674af749e
fmh-DEV <fm.hildebrand@gmail.com>
2024-03-23 22:10:57 -0300

    2:14:00

--- app/(tabs)/calls/index.tsx
+++ app/(tabs)/calls/index.tsx
@@ -15,6 +15,12 @@ import calls from '@/assets/data/calls.json';
 import { defaultStyles } from '@/constants/Styles';
 import { format } from 'date-fns';
 import { SegmentedControl } from '@/components/SegmentedControl';
+import Animated, {
+  CurvedTransition,
+  FadeInUp,
+  FadeOut,
+  FadeOutUp,
+} from 'react-native-reanimated';
 
 const Page = () => {
   //
@@ -32,6 +38,8 @@ const Page = () => {
       setItems(calls.filter((item) => item.missed));
     }
   }, [selectedOption]);
+  //
+  const transition = CurvedTransition.delay(100);
 
   return (
     <View style={{ flex: 1, backgroundColor: Colors.background }}>
@@ -58,53 +66,59 @@ const Page = () => {
         contentContainerStyle={{ paddingBottom: 40 }}
       >
         <View style={defaultStyles.block}>
-          <FlatList
+          <Animated.FlatList
             data={items}
             scrollEnabled={false}
             keyExtractor={(item) => item.id.toString()}
             ItemSeparatorComponent={() => (
               <View style={defaultStyles.separator} />
             )}
-            renderItem={({ item }) => (
-              <View style={[defaultStyles.item]}>
-                <Image source={{ uri: item.img }} style={styles.avatar} />
-                <View style={{ flex: 1, gap: 2 }}>
-                  <Text
+            itemLayoutAnimation={FadeOutUp}
+            renderItem={({ item, index }) => (
+              <Animated.View
+                entering={FadeInUp.delay(index * 20)}
+                exiting={FadeOut}
+              >
+                <View style={[defaultStyles.item]}>
+                  <Image source={{ uri: item.img }} style={styles.avatar} />
+                  <View style={{ flex: 1, gap: 2 }}>
+                    <Text
+                      style={{
+                        fontSize: 18,
+                        color: item.missed ? Colors.red : '#000',
+                      }}
+                    >
+                      {item.name}
+                    </Text>
+                    <View style={{ flexDirection: 'row', gap: 4 }}>
+                      <Ionicons
+                        name={item.video ? 'videocam' : 'call'}
+                        size={16}
+                        color={Colors.gray}
+                      />
+                      <Text style={{ flex: 1, color: Colors.gray }}>
+                        {item.incoming ? 'Incoming' : 'Outgoing'}
+                      </Text>
+                    </View>
+                  </View>
+                  <View
                     style={{
-                      fontSize: 18,
-                      color: item.missed ? Colors.red : '#000',
+                      flexDirection: 'row',
+                      gap: 6,
+                      alignItems: 'center',
                     }}
                   >
-                    {item.name}
-                  </Text>
-                  <View style={{ flexDirection: 'row', gap: 4 }}>
-                    <Ionicons
-                      name={item.video ? 'videocam' : 'call'}
-                      size={16}
-                      color={Colors.gray}
-                    />
-                    <Text style={{ flex: 1, color: Colors.gray }}>
-                      {item.incoming ? 'Incoming' : 'Outgoing'}
+                    <Text style={{ color: Colors.gray }}>
+                      {format(item.date, 'MM.dd.yy')}
                     </Text>
+                    <Ionicons
+                      name='information-circle-outline'
+                      size={24}
+                      color={Colors.primary}
+                    />
                   </View>
                 </View>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    gap: 6,
-                    alignItems: 'center',
-                  }}
-                >
-                  <Text style={{ color: Colors.gray }}>
-                    {format(item.date, 'MM.dd.yy')}
-                  </Text>
-                  <Ionicons
-                    name='information-circle-outline'
-                    size={24}
-                    color={Colors.primary}
-                  />
-                </View>
-              </View>
+              </Animated.View>
             )}
           />
         </View>
