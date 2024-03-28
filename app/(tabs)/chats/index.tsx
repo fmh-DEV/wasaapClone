7310dc086c84f71ca070ecf3823faa4bbfee3d2e
fmh-DEV <fm.hildebrand@gmail.com>
2024-03-28 16:08:42 -0300

    Section List with Alphabet Scroll. 2:55:00

--- app/(tabs)/chats/index.tsx
+++ app/(tabs)/chats/index.tsx
@@ -1,13 +1,24 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import React from 'react';
+import chats from '@/assets/data/chats.json';
+import { defaultStyles } from '@/constants/Styles';
+import ChatRow from '@/components/ChatRow';
 
 const Page = () => {
   return (
     <ScrollView
       contentInsetAdjustmentBehavior='automatic'
-      contentContainerStyle={{ paddingBottom: 40 }}
+      contentContainerStyle={{ paddingBottom: 40, backgroundColor: '#fff' }}
     >
-      <Text>INSIDE</Text>
+      <FlatList
+        scrollEnabled={false}
+        data={chats}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+          <View style={[defaultStyles.separator, { marginLeft: 90 }]} />
+        )}
+        renderItem={({ item }) => <ChatRow {...item} />}
+      />
     </ScrollView>
   );
 };
